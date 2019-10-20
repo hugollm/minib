4d0419323da1cd53b94f212f35935a9a18e6a78e
Hugo Mota <hugo.txt@gmail.com>
2019-10-20 13:31:24 -0300

    Add support for dev and build commands on bin script

--- src/bin.js
+++ src/bin.js
@@ -1,23 +1,38 @@
 const exec = require('child_process').exec;
 
 const log = require('./log');
-const tasks = require(process.cwd() + '/minib.config.js');
+const config = require(process.cwd() + '/minib.config.js');
 
 
 main();
 
 function main() {
-    build();
+    let args = process.argv.slice(2);
+    let mode = args[0];
+    if (mode != 'dev' && mode != 'build')
+        return log.error('Invalid command: ' + mode);
+    log.notice(mode.toUpperCase());
+    runPipeline(mode);
 }
 
-function build() {
-    for (let name in tasks) {
-        tasks[name].name = name;
-        handleTask(tasks[name]);
-    }
+function runPipeline(mode) {
+    let tasks = getTasks(mode);
+    for (let i in tasks)
+        handleTask(tasks[i]);
+}
+
+function getTasks(mode) {
+    return Object.keys(config).map(name => {
+        task = config[name];
+        task.name = name;
+        task.cmd = task[mode];
+        return task;
+    });
 }
 
 function handleTask(task) {
+    if (!task.cmd)
+        return log.debug(task.name + ' SKIPPED');
     if (task.after)
         scheduleTask(task);
     else
@@ -30,8 +45,8 @@ function scheduleTask(task) {
 }
 
 function executeTask(task) {
-    log.debug(task.name + ' RUNNING');
-    exec(task.build, (error, stdout, stderr) => {
+    log.info(task.name + ' RUNNING');
+    exec(task.cmd, (error, stdout, stderr) => {
         if (error)
             taskError(task, stdout + stderr);
         else
